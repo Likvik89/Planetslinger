d6eec2a707a7864bc4807e67958f3c3eee42bd96
Likvik89 <erik@elfisk.dk>
2023-11-14 11:12:57 +0100

    extra mechanics

--- Scripts/Playership.gd
+++ Scripts/Playership.gd
@@ -3,9 +3,30 @@ extends RigidBody2D
 @export var THRUST_FORCE = 25  # The force applied when moving forward
 var thrust_force_multiplier = 10000
 const rotationspeed = 250.0
+
 @export var G = 1000  # Gravitational constant
+@export var startmass = 700
+@export var gravitychange = 50
+
+@export var maxfuel := 200
+var fuel = 200
+
+
 
 func _integrate_forces(delta):
+	
+	if Input.is_action_just_pressed("gravityup"):
+		self.mass += gravitychange
+	if Input.is_action_just_pressed("gravitydown"):
+		self.mass -= gravitychange
+	
+	if self.mass < startmass:
+		self.mass = startmass
+	if self.mass > startmass*2:
+		self.mass = startmass*2
+	
+	print(self.mass)
+	
 	var bodies = get_tree().get_nodes_in_group("planets")
 	for body in bodies:
 		if body != self:
@@ -13,7 +34,7 @@ func _integrate_forces(delta):
 			var force_magnitude = (G * self.mass * body.mass) / (distance * distance)
 			var direction = (body.global_position - self.global_position).normalized()
 			var force = direction * force_magnitude # combine force magnitude and direction
-			apply_central_force(force) 
+			apply_central_force(force)
 			
 	look_at(get_global_mouse_position())
 	# Apply thrust when moving forward
@@ -25,4 +46,19 @@ func _integrate_forces(delta):
 	if Input.is_action_pressed("move_back"):
 		var direction = Vector2(cos(rotation), sin(rotation))  # Calculate backward direction based on rotation
 		apply_central_impulse(-direction * THRUST_FORCE * thrust_force_multiplier * delta.step)
-
+	
+	if Input.is_action_pressed("move_boost") and fuel > 0:
+		THRUST_FORCE = 50
+		if fuel >= 2:
+			fuel -=2
+	else:
+		THRUST_FORCE = 25
+		fuel += 2
+	
+	if fuel > maxfuel:
+		fuel = maxfuel
+	if fuel < 0:
+		fuel = 0
+	
+	print(THRUST_FORCE)
+	print(fuel)
