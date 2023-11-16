b45e7665b0bc9a712fe221c645009346b27da496
Likvik89 <erik@elfisk.dk>
2023-11-16 14:06:37 +0100

    enmyv1

---
+++ Scripts/Enemy.gd
@@ -0,0 +1,18 @@
+extends RigidBody2D
+
+@export var G = 1000  # Gravitational constant
+@export var health = 400
+
+func _on_Area2D_body_enter(body):
+	print(str('Body entered: ', body.get_name()))
+
+
+func _integrate_forces(state):
+	var bodies = get_tree().get_nodes_in_group("planets")
+	for body in bodies:
+		if body != self:
+			var distance = self.global_position.distance_to(body.global_position)
+			var force_magnitude = (G * self.mass * body.mass) / (distance * distance)
+			var direction = (body.global_position - self.global_position).normalized()
+			var force = direction * force_magnitude # combine force magnitude and direction
+			apply_central_force(force)
