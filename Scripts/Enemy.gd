3771a297de2a51cba8fd6f58445e6ac6115a3c12
Likvik89 <erik@elfisk.dk>
2023-11-19 20:09:40 +0100

    repulsion added

--- Scripts/Enemy.gd
+++ Scripts/Enemy.gd
@@ -4,12 +4,6 @@ extends RigidBody2D
 @export var health = 400
 @export var player : RigidBody2D
 
-func _process(delta):
-	look_at(player.position)
-	var direction = Vector2(cos(rotation), sin(rotation))  # Calculate forward direction based on rotation
-	print(direction)
-	apply_central_force(direction * 350)
-
 
 func _integrate_forces(state):
 	var bodies = get_tree().get_nodes_in_group("planets")
@@ -20,9 +14,16 @@ func _integrate_forces(state):
 			var direction = (body.global_position - self.global_position).normalized()
 			var force = direction * force_magnitude # combine force magnitude and direction
 			apply_central_force(force)
+	
+	
+	var player_direction = (player.position - position).normalized()
+	var player_distance = position.distance_to(player.position)
+	var rotation_angle = atan2(player_direction.y, player_direction.x)
+	rotation = rotation_angle
+	apply_central_force(player_direction*50)
 
 
-func _on_area_2d_body_entered(body):
-	print("OUCH!")
-	if body.is_in_group('planets'):
-		print(str('Body entered: ', body.get_name()))
+#func _on_area_2d_body_entered(body):
+	#print("OUCH!")
+	#if body.is_in_group('planets'):
+		#print(str('Body entered: ', body.get_name()))
--- Scripts/Playership.gd
+++ Scripts/Playership.gd
@@ -1,33 +1,58 @@
 extends RigidBody2D
 
+#Movement variables
 @export var THRUST_FORCE = 25  # The force applied when moving forward
 var thrust_force_multiplier = 10000
 const rotationspeed = 250.0
 
+#Gravity variables
 @export var G = 1000  # Gravitational constant
 @export var startmass = 700
 @export var gravitychange = 50
 
+#Boosting variables
 @export var maxfuel := 200
 var fuel = 200
 
+#Repulsion variables
+@export var maxrepenergi := 400
+var repenergi = 400
+var repenergirecovery = 1
+
+#Combat variables
 @export var health = 600
 
 
-	
+
+
 func _on_area_2d_body_entered(body):
-	if body.is_in_group('planets'):
-		health -= 2
-	print(health)
-
-
-
+	if body.is_in_group('planets') and body != self:
+		health -= body.get_angular_velocity () +  self.get_angular_velocity ()
+	#print(health)
 
 
 func _integrate_forces(delta):
 	
-	var force = 2
+	#Repulsing
+	if Input.is_action_pressed("Repulse"):
+		if repenergi > maxrepenergi-2:
+			for body in $"Repulsion field".get_overlapping_bodies():
+				if body != self:
+					var distance = self.global_position.distance_to(body.global_position)
+					var direction = (body.global_position - self.global_position).normalized()
+					body.apply_central_impulse((direction * 100000 ))
+					repenergi = 0
+
+
+	if not Input.is_action_pressed("Repulse"):
+		repenergi += repenergirecovery
+		if repenergi > maxrepenergi:
+			repenergi = maxrepenergi
+	print(repenergi)
 	
+	
+	
+	#Changing gravity
 	if Input.is_action_just_pressed("gravityup"):
 		self.mass += gravitychange
 	if Input.is_action_just_pressed("gravitydown"):
@@ -38,30 +63,29 @@ func _integrate_forces(delta):
 	if self.mass > startmass*2:
 		self.mass = startmass*2
 	
-
-	
+	#Gravity calculation
 	var bodies = get_tree().get_nodes_in_group("planets")
 	for body in bodies:
 		if body != self:
 			var distance = self.global_position.distance_to(body.global_position)
 			var force_magnitude = (G * self.mass * body.mass) / (distance * distance)
 			var direction = (body.global_position - self.global_position).normalized()
-			force = direction * force_magnitude # combine force magnitude and direction
+			var force = direction * force_magnitude # combine force magnitude and direction
 			apply_central_force(force)
-			
-			
-			
+	
+	
+	#Movement
 	look_at(get_global_mouse_position())
-	# Apply thrust when moving forward
 	if Input.is_action_pressed("move_forward"):
-		var direction = Vector2(cos(rotation), sin(rotation))  # Calculate forward direction based on rotation
+		var direction = Vector2(cos(rotation), sin(rotation)) 
 		apply_central_impulse(direction * THRUST_FORCE * thrust_force_multiplier * delta.step)
-
-	# Apply reverse thrust when moving backward
+	
 	if Input.is_action_pressed("move_back"):
 		var direction = Vector2(cos(rotation), sin(rotation))  # Calculate backward direction based on rotation
 		apply_central_impulse(-direction * THRUST_FORCE * thrust_force_multiplier * delta.step)
 	
+	
+	#Boosting
 	if Input.is_action_pressed("move_boost") and fuel > 0:
 		THRUST_FORCE = 50
 		if fuel >= 2:
