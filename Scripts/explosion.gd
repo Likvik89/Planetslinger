78b7de8ea1579c64ededfac6442c070cb217fa9d
Likvik89 <erik@elfisk.dk>
2023-12-13 13:51:15 +0100

    sfx

--- Scripts/explosion.gd
+++ Scripts/explosion.gd
@@ -3,7 +3,9 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	
 	self.play("default")
+	$boom.play()
 	if self.frame == 6:
 		print("Exploded")
 		queue_free()
--- Scripts/gameover.gd
+++ Scripts/gameover.gd
@@ -3,12 +3,19 @@ extends Control
 @export var player : PackedScene
 
 func _ready():
+	$ded.play()
 	$Score.text = str(Score.score)
 	if Score.score > Score.highscore:
 		Score.highscore = Score.score
 	$Highscore.text = str(Score.highscore)
 	Score.score = 0
-	
+
+
 
 func _on_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/spaece.tscn")
+
+
+func _on_main_menu_pressed():
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+	queue_free()
