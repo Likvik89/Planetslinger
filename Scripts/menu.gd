906c31a4bbb0ac7103b96c4714d27beaf7166cb0
Likvik89 <erik@elfisk.dk>
2023-12-14 10:00:17 +0100

    ui working

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -10,5 +10,5 @@ func _on_quit_pressed():
 	get_tree().quit()
 
 
-#func _on_optoins_pressed():
-#	get_tree().change_scene_to_file("res://Scenes/options.tscn")
+func _on_optoins_pressed():
+	get_tree().change_scene_to_file("res://Scenes/options.tscn")
--- Scripts/options.gd
+++ Scripts/options.gd
@@ -2,5 +2,5 @@ extends Control
 
 
 
-#func _on_back_pressed():
-	#get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+func _on_back_pressed():
+	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
